df6ede19a6db74d081b7145f4f43d13d7bdeb16c
Moussa-13 <m.amzat@alustudent.com>
2022-07-19 21:45:27 +0200

    Task 1 - if

--- 1_if_task.py
+++ 1_if_task.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3 
+# Given an integer, m, perform the following conditional actions
+# Print "Weird" if even number
+# Print "Not Weird" if odd number in range(2, 5)
+# Print "Weird" if m is odd number in range(6, 20)
+# Print "Not Weird" if odd and greater than 20
 
 m = int(input("Enter a number: "))
 if (m % 2) == 0:
     print("{} is Weird".format(m))
-    
-for m in range(2, 5):
-    if (m % 2) != 0:
+else:
+    if 2 <= m < 5:
         print("{} is Not Weird".format(m))
 
-for m in range(6, 20):
-    if (m % 2) != 0:
+    elif 6 <= m < 20 or 2 <= m < 5 or m > 20:
         print("{} is Not Weird".format(m))
