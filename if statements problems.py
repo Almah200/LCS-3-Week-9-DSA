b650689fbaf3c3720100a9491ec47c2c5f798977
Almarat Arnu Ngutulu <99120810+Almah200@users.noreply.github.com>
2022-07-20 12:20:30 +0300

    if statements problem 1

---
+++ if statements problems.py
@@ -0,0 +1,9 @@
+m = int(input('Please enter a number: '))
+if m % 2 == 0:
+    print(' Weird')
+elif m % 2 != 0 and m in range(2, 6):
+        print('Not Weird')
+elif m % 2 != 0 and m in range(6, 21):
+    print('Weird')
+elif m % 2 != 0 and m > 20:
+    print('Not Weird')
