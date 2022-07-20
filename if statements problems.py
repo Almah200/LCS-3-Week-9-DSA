540fa2571793cab76bedf73d16a5a7ad53e4a32e
Almarat Arnu Ngutulu <99120810+Almah200@users.noreply.github.com>
2022-07-20 12:36:09 +0300

    if statements problem 2 and made some changes on if statements problems

--- if statements problems.py
+++ if statements problems.py
@@ -5,5 +5,6 @@ elif m % 2 != 0 and m in range(2, 6):
         print('Not Weird')
 elif m % 2 != 0 and m in range(6, 21):
     print('Weird')
-elif m % 2 != 0 and m > 20:
+else:
     print('Not Weird')
+
